3054e9e8b094703b2acd86459915bc2cea0281b5
vinicius777 <viniciusguedes63@gmail.com>
2021-12-20 14:20:00 -0300

    Arrumando interface

--- lib/Screens/home_page.dart
+++ lib/Screens/home_page.dart
@@ -7,45 +7,40 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Container(
-      child: topBar,
+      color: Colors.white,
+      child: ListView(
+        children: <Widget>[
+          SizedBox(
+            width: double.infinity,
+            height: 120,
+            child: Container(
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Padding(padding: EdgeInsets.only(left: 20, top: 70)),
+                      Icon(Icons.person),
+                      Padding(padding: EdgeInsets.only(right: 120)),
+                      Icon(Icons.mail),
+                      Icon(Icons.help)
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Padding(padding: EdgeInsets.only(left: 20)),
+                      Text(
+                        'Olá,Vinicius',
+                        style: TextStyle(color: Colors.white, fontSize: 20),
+                      )
+                    ],
+                  )
+                ],
+              ),
+              color: Colors.deepPurple,
+            ),
+          )
+        ],
+      ),
     ));
   }
 }
-
-final topBar = Container(
-  padding: EdgeInsets.fromLTRB(20, 40, 70, 70),
-  child: Row(
-    mainAxisSize: MainAxisSize.min,
-    children: [
-      Column(
-        children: [
-          Icon(
-            Icons.person,
-            color: Colors.white,
-          ),
-          const Text('Olá Vinicius'),
-        ],
-      ),
-      Padding(padding: EdgeInsets.only(right: 150)),
-      Icon(
-        Icons.remove_red_eye,
-        color: Colors.white,
-      ),
-      Padding(
-        padding: EdgeInsets.only(right: 20),
-      ),
-      Icon(
-        Icons.message,
-        color: Colors.white,
-      ),
-      Padding(padding: EdgeInsets.only(right: 20)),
-      Expanded(
-        child: Icon(
-          Icons.help,
-          color: Colors.white,
-        ),
-      ),
-    ],
-  ),
-  color: Colors.purple,
-);
