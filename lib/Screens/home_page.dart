98ada53bce854e353e6028c963c6f2cf547fe617
vinicius777 <viniciusguedes63@gmail.com>
2021-12-21 13:57:43 -0300

    App bar /conta/valor

--- lib/Screens/home_page.dart
+++ lib/Screens/home_page.dart
@@ -9,36 +9,109 @@ class HomePage extends StatelessWidget {
         body: Container(
       color: Colors.white,
       child: ListView(
+        scrollDirection: Axis.vertical,
         children: <Widget>[
+          //inicio da appbar
           SizedBox(
             width: double.infinity,
-            height: 120,
+            height: 133,
             child: Container(
+              color: Color(0xff830ad1),
               child: Column(
                 children: [
                   Row(
                     children: [
                       Padding(padding: EdgeInsets.only(left: 20, top: 70)),
-                      Icon(Icons.person),
-                      Padding(padding: EdgeInsets.only(right: 120)),
-                      Icon(Icons.mail),
-                      Icon(Icons.help)
+                      CircleAvatar(
+                        child: Image.asset(
+                          'assets/user.png',
+                          width: 20,
+                          height: 20,
+                        ),
+                        backgroundColor: Color(0xff9b3bda),
+                        radius: 22,
+                      ),
+                      Padding(padding: EdgeInsets.only(right: 140)),
+                      Image.asset(
+                        'assets/eye_open.png',
+                        width: 25,
+                        height: 25,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(right: 25),
+                      ),
+                      Icon(
+                        Icons.help_outline,
+                        color: Colors.white,
+                      ),
+                      Padding(padding: EdgeInsets.only(right: 25)),
+                      Image.asset(
+                        'assets/add-message.png',
+                        width: 22,
+                        height: 22,
+                      )
                     ],
                   ),
                   Row(
                     children: [
-                      Padding(padding: EdgeInsets.only(left: 20)),
+                      Padding(padding: EdgeInsets.only(left: 22, top: 40)),
                       Text(
-                        'Olá,Vinicius',
-                        style: TextStyle(color: Colors.white, fontSize: 20),
+                        'Olá, Vinicius',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 19,
+                            fontWeight: FontWeight.w600),
                       )
                     ],
                   )
                 ],
               ),
-              color: Colors.deepPurple,
             ),
+          ),
+          //fim da appbar
+          SizedBox(
+            width: 20,
+          ),
+          //inicio do corpo : conta e valor
+          Column(
+            children: [
+              Padding(padding: EdgeInsets.only(top: 22)),
+              Row(
+                children: [
+                  Padding(padding: EdgeInsets.only(left: 20)),
+                  TextButton(
+                      onPressed: () {},
+                      child: Row(
+                        children: [
+                          Text(
+                            'Conta',
+                            style: TextStyle(
+                                fontSize: 22,
+                                color: Colors.black,
+                                fontWeight: FontWeight.w500),
+                          ),
+                          Padding(padding: EdgeInsets.only(left: 210)),
+                          Icon(
+                            Icons.chevron_right,
+                            size: 30,
+                            color: Color(0xff6e717a),
+                          )
+                        ],
+                      ))
+                ],
+              ),
+              Row(
+                children: [
+                  Padding(padding: EdgeInsets.only(top: 30, left: 30)),
+                  Text(
+                    'R\$ 10.000,00',
+                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.w500),
+                  )
+                ],
+              )
+            ],
           )
+          //fim da conta e valor
         ],
       ),
     ));
